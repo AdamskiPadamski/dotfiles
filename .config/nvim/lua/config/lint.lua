d4917c337bea48178733c6f2f4daa09a39523a49
Adam Wright <adam.wright3@sky.uk>
2025-07-30 20:04:10 +0100

    add conform and orgmode

--- .config/nvim/lua/config/lint.lua
+++ .config/nvim/lua/config/lint.lua
@@ -1,1 +1,28 @@
-require("lint").linters_by_ft = {}
+require("lint").linters_by_ft = {
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescriptreact = { "eslint_d" }
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    javascript = { "eslint_d" },
+    typescript = { "eslint_d" },
+    javascriptreact = { "eslint_d" },
+    typescriptreact = { "eslint_d" }
+  },
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
